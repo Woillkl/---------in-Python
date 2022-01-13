8c33218595584b26e24e13cad75ca85a54c226d0
Wooil Shin <tlsdndlfdlek@naver.com>
2022-01-12 21:20:00 -0700

    Homework

---
+++ 프로그래밍 기초 in Python/GroupStudy/for_space_count.py
@@ -0,0 +1,9 @@
+string = "This is NGL Transportation"
+
+blank_count = 0
+for i in range(len(string)-1) :
+    print(string[i])
+    if string[i] == " " :
+        blank_count += 1
+
+print(f"Blank Space : {blank_count}")
